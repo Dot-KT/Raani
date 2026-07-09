4d87079f2c7592915a640ab30e80cc25e0cf7646
Dot-KT <7devanle@gmail.com>
2026-07-09 12:46:41 +0100

    added description in my enums and added the plastic and giant paint in measurement)

--- src/main/java/Raani/model/Measurement.java
+++ src/main/java/Raani/model/Measurement.java
@@ -1,8 +1,19 @@
 package Raani.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public enum Measurement {
     KG,
+    @Schema(description = "5 dericas")
     PAINT,
+    @Schema(description = "16 dericas")
+    PLASTIC,
+    @Schema(description = "contains 5 paints and 1 derica, 26 derica")
+    GIANT_PAINT,
+    @Schema (description = "a bigger size of poultry")
     BIG,
-    SMALL
+    @Schema (description = "a smaller size of poultry")
+    SMALL;
+
+
 }
